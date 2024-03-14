7bf293a5acdd5ea5559fd1a14733cb16d5ed2eb8
Yuzu_Knsor <yuzuknsor@outlook.com>
2024-03-14 06:53:04 +0000

    nothing

--- C/NCRE-R2-CPP/1.cpp
+++ C/NCRE-R2-CPP/1.cpp
@@ -15,5 +15,67 @@ std::string成员函数
         在这个例子中，cstr是一个指向字符数组的指针，这个字符数组包含了"Hello, world!"和一个'\0'。C++风格则无'\0'。
     operator[]: 用于访问字符串中的单个字符。
 
+    strcpy-s: 复制字符串
+    strcat-s: 连接字符串
+    strlen-s: 返回字符串长度
+    strcmp-s: 比较字符串
+    strchr-s: 查找字符
+    strstr-s: 查找字符串
+    strtok-s: 分割字符串
 
+    auto
+    case
+    default
+    do
+    enum
+    extern
+    static
+    union
+    volatile
+    catch
+    const_cast
+    delete
+    dynamic_cast
+    explicit
+    friend
+    inline
+    mutable
+    new
+    operator
+    reinterpret_cast
+    static_cast
+    template
+    this
+    throw
+    try
+    typeid
+    typename
+    typedef
+    using
+    virtual
+    wchar_t
+
+    定义与声明
+
+    \a      7       响铃
+    \b      8       退格
+    \f      12      换页
+    \n      10      换行
+    \r      13      回车
+    \t      9       水平制表符
+    \v      11      垂直制表符
+    \\      92      反斜杠
+    \'      39      单引号
+    \"      34      双引号
+    \?      63      问号
+    \0      0       空字符
+    \ddd            1到3位八进制数所代表的任意字符
+    \xhh            1到2位十六进制数所代表的任意字符
+    \uhhhh          4位十六进制数所代表的Unicode字符
+    \Uhhhhhhhh      8位十六进制数所代表的Unicode字符
+
+    引用和迭代
+    引用传参
+
+    
 */
